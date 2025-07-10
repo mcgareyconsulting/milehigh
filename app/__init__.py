c3dc67d47e35e7ae9932d477c06d1cd8c90fe368
mcgareyconsulting <mcgareyconsulting@gmail.com>
2025-07-10 08:41:56 -0600

    Refactoring calling of the trello webhook.

--- app/__init__.py
+++ app/__init__.py
@@ -1,5 +1,6 @@
 from flask import Flask, request
 from .config import Config as cfg
+from routes.trello_webhook import trello_bp
 
 
 def create_app():
@@ -7,27 +8,8 @@ def create_app():
 
     @app.route("/")
     def index():
-        message = f"Welcome to TSI {cfg.TRELLO_BOARD_ID} {cfg.TRELLO_API_KEY} {cfg.TRELLO_TOKEN} {cfg.AZURE_CLIENT_ID} {cfg.AZURE_CLIENT_SECRET} {cfg.AZURE_TENANT_ID} {cfg.WEBHOOK_URL}"
+        return "Trello SharePoint Integration is running!"
 
-        return message
-
-    @app.route("/trello/webhook", methods=["HEAD", "POST"])
-    def trello_webhook():
-        if request.method == "HEAD":
-            return "", 200  # Trello's webhook validation ping
-
-        data = request.json
-        action = data.get("action", {})
-        if action.get("type") == "updateCard":
-            movement = action.get("data", {})
-            if "listBefore" in movement and "listAfter" in movement:
-                card_name = movement["card"]["name"]
-                before = movement["listBefore"]["name"]
-                after = movement["listAfter"]["name"]
-                print(
-                    f"[Render] Card '{card_name}' moved from '{before}' to '{after}'."
-                )
-
-        return "", 200
+    app.register_blueprint(trello_bp, url_prefix="/trello")
 
     return app
---
+++ app/routes/trello_webhook.py
@@ -0,0 +1,21 @@
+from flask import Blueprint, request
+
+trello_bp = Blueprint("trello", __name__)
+
+
+@trello_bp.route("/webhook", methods=["HEAD", "POST"])
+def trello_webhook():
+    if request.method == "HEAD":
+        return "", 200  # Trello's webhook validation ping
+
+    data = request.json
+    action = data.get("action", {})
+    if action.get("type") == "updateCard":
+        movement = action.get("data", {})
+        if "listBefore" in movement and "listAfter" in movement:
+            card_name = movement["card"]["name"]
+            before = movement["listBefore"]["name"]
+            after = movement["listAfter"]["name"]
+            print(f"[Render] Card '{card_name}' moved from '{before}' to '{after}'.")
+
+    return "", 200
