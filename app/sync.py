ece4831502f53c40bcb0459ed53fdeb4379f9f46
mcgareyconsulting <mcgareyconsulting@gmail.com>
2025-07-22 10:38:57 -0600

    Collecting name.

--- app/sync.py
+++ app/sync.py
@@ -1,7 +1,9 @@
 def sync_from_trello(data):
+    """Sync data from Trello to OneDrive based on the webhook payload."""
     print(data)
-    # # Parse Trello webhook payload
-    # card_id = data.get("action", {}).get("data", {}).get("card", {}).get("id")
+    # Parse Trello webhook payload
+    card_id = data.get("action", {}).get("data", {}).get("card", {}).get("text")
+    print("Card ID:", card_id)
     # # Call OneDrive-related functions here
     # from app.sheets import update_excel_with_trello_data
 
