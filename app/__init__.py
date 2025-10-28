37244dd7dcd2d23484ffe8612c50cbd98e9f9f2f
mcgareyconsulting <mcgareyconsulting@gmail.com>
2025-10-28 15:39:10 -0600

    Updating scheduler.

--- app/__init__.py
+++ app/__init__.py
@@ -106,7 +106,7 @@ def init_scheduler(app):
     scheduler.add_job(
         func=scheduled_run,
         trigger="cron",
-        minute="38",
+        minute="0",
         hour="*",
         id="onedrive_poll",
         name="OneDrive Polling Job",
