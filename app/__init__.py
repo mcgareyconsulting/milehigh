bc7e8ab86192d8a2e3c0b80b7853d663f42a8c4c
mcgareyconsulting <mcgareyconsulting@gmail.com>
2025-11-03 12:27:06 -0700

    Fixing some trello mapping.

--- app/__init__.py
+++ app/__init__.py
@@ -106,7 +106,7 @@ def init_scheduler(app):
     scheduler.add_job(
         func=scheduled_run,
         trigger="cron",
-        minute="46",
+        minute="49",
         hour="*",
         id="onedrive_poll",
         name="OneDrive Polling Job",
