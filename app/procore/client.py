a91bf0f14e4df2bfadaf27e001af65c8a1f7b45d
mcgareyconsulting <mcgareyconsulting@gmail.com>
2026-02-25 16:25:14 -0700

    Updating procore client for sandbox deploy.

--- app/procore/client.py
+++ app/procore/client.py
@@ -9,9 +9,9 @@ def get_procore_client():
     if _client is None:
         from app.config import Config as cfg
         # Dev
-        _client = ProcoreAPI(cfg.PROD_PROCORE_CLIENT_ID, cfg.PROD_PROCORE_CLIENT_SECRET, cfg.PROCORE_DEV_WEBHOOK_URL)
+        # _client = ProcoreAPI(cfg.PROD_PROCORE_CLIENT_ID, cfg.PROD_PROCORE_CLIENT_SECRET, cfg.PROCORE_DEV_WEBHOOK_URL)
         # Prod
         # _client = ProcoreAPI(cfg.PROD_PROCORE_CLIENT_ID, cfg.PROD_PROCORE_CLIENT_SECRET, cfg.PROCORE_PROD_WEBHOOK_URL)
         # Sandbox
-        # _client = ProcoreAPI(cfg.PROD_PROCORE_CLIENT_ID, cfg.PROD_PROCORE_CLIENT_SECRET, cfg.PROCORE_SANDBOX_WEBHOOK_URL)
+        _client = ProcoreAPI(cfg.PROD_PROCORE_CLIENT_ID, cfg.PROD_PROCORE_CLIENT_SECRET, cfg.PROCORE_SANDBOX_WEBHOOK_URL)
     return _client
